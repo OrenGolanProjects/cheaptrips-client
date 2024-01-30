7c87d1af3e455368343b765a0113bc6cb1a981ad
oren-golan <111655672+oren-golan@users.noreply.github.com>
2024-01-30 07:24:21 +0200

    modified:   utils/database.js

--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://admin:admin@cheaptripsclient.73m6xws.mongodb.net/cheaptrips?retryWrites=true&w=majority&ssl=true')
+    MongoClient.connect('mongodb+srv://admin:admin@cheaptripsclient.73m6xws.mongodb.net/cheaptrips?retryWrites=true&w=majority')
         .then(client => {
             console.log('MongoDB Connection!');
             _db = client.db(); // Set _db to the client's database
